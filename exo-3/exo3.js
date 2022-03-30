6e3ec32ec09a412e97aff259497ec9dde8478050
DianeBinsztok <diane.binsztok@le-campus-numerique.fr>
2022-03-30 15:37:45 +0200

    exo-3: 4 - checking player input

--- exo-3/exo3.js
+++ exo-3/exo3.js
@@ -4,7 +4,13 @@ var playerInput = prompt("Your choice ( rock, paper, scissors) ?");
 // traiter l'input
 function getPlayerChoice(input) {
     let lowerCaseInput = input.toLowerCase();
-    console.log(lowerCaseInput);
+    if ((lowerCaseInput == "rock") || (lowerCaseInput == "paper") || (lowerCaseInput == "scissors")) {
+        playerInput = lowerCaseInput;
+    } else {
+        input = prompt("Your choice can only be: rock, paper or scissors.");
+        getPlayerChoice(input)
+    }
 }
 
 getPlayerChoice(playerInput);
+console.log("You chose:" , playerInput);
