629d4591742344c4cfeedbd5d153283535e66587
DianeBinsztok <diane.binsztok@le-campus-numerique.fr>
2022-03-30 15:47:47 +0200

    exo-3: 4 - computer's choice

--- exo-3/exo3.js
+++ exo-3/exo3.js
@@ -1,16 +1,29 @@
-// demander le choix utilisateur
+// Demander le choix utilisateur:
 var playerInput = prompt("Your choice ( rock, paper, scissors) ?");
 
-// traiter l'input
+// Traiter l'input:
 function getPlayerChoice(input) {
     let lowerCaseInput = input.toLowerCase();
     if ((lowerCaseInput == "rock") || (lowerCaseInput == "paper") || (lowerCaseInput == "scissors")) {
         playerInput = lowerCaseInput;
+        console.log("You chose:", playerInput);
     } else {
         input = prompt("Your choice can only be: rock, paper or scissors.");
         getPlayerChoice(input)
     }
 }
 
+// L'ordinateur choisit:
+function getComputerChoice() {
+    // Les choix possibles:
+    const choices = ["rock", "paper", "scissors"];
+    // choix randomisé
+    let random = Math.round(Math.random(0, 2));
+    let computerChoice = choices[random];
+    console.log("I chose:", computerChoice);
+}
+
 getPlayerChoice(playerInput);
-console.log("You chose:" , playerInput);
+getComputerChoice();
+
+
