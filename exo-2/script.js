299fa7f179efe9f3f165cab9895d1e1f55e2ff5c
DianeBinsztok <diane.binsztok@le-campus-numerique.fr>
2022-03-30 14:45:03 +0200

    exo-2: 3 - refactor

--- exo-2/script.js
+++ exo-2/script.js
@@ -6,7 +6,7 @@ const myDate = new Date();
 console.log(myDate);
 // ça renvoie la date et l'heure actuelles : "Wed Mar 30 2022 11:48:36 GMT+0200 (heure d’été d’Europe centrale)"
 
-
+// Les messages
 const weekMsg = "Nous sommes en semaine!";
 const weekendMsg = "C'est le weekend!";
 
@@ -16,24 +16,27 @@ let today = myDate.getDay();
 // Cette heure:
 let now = myDate.getHours();
 
-// Je vérifie l'heure
-function fridayNightFever(hour) {
-    if (hour >= 17) {
+// Je vérifie si c'est le weekend en fonction du jour et de l'heure
+function checkIfWeekend(day, hour) {
+    if ((day === 7) || (day === 0)) {
+        return true;
+    }
+    if ((day === 6 && (hour >= 17))) {
         return true;
     } else {
         return false;
     }
 }
-const weekendStart = fridayNightFever(now);
+// Je stocke le résultat dans un booléen
+const weekend = checkIfWeekend(today, now);
 
-// en fonction du jour et de la condition weekendStart, je vérifie si c'est le weekend:
-function checkWeekDay(day) {
-    // Si nous sommes samedi OU dimanche OU vendredi après 17h
-    if ((day === 7) || (day === 0) || (day===6 && weekendStart)) {
+// J'envoie un message en fonction de ce booléen
+function weekendAlarm(boolCheck){
+    if (boolCheck) {
         console.log(weekendMsg);
-    } else {
+    } else{
         console.log(weekMsg);
     }
 }
 
-checkWeekDay(0);
+weekendAlarm(weekend);
