e34abeb48e5aaa0128dce84bcc4fc5bfcbf2a9e2
DianeBinsztok <diane.binsztok@le-campus-numerique.fr>
2022-03-30 14:00:11 +0200

    exo-2: 1 & 2 - instance of Date()

--- exo-2/script.js
+++ exo-2/script.js
@@ -1,2 +1,14 @@
 // you can write js here
-console.log('exo-2');
+//console.log('exo-2');
+
+// j'instancie la classe Date() qui existe déjà en JS
+const myDate = new Date();
+console.log(myDate);
+// ça renvoie la date et l'heure actuelles : "Wed Mar 30 2022 11:48:36 GMT+0200 (heure d’été d’Europe centrale)"
+
+
+const weekMsg = "Nous sommes en semaine!";
+const weekendMsg = "C'est le weekend!";
+
+let today = myDate.getDay();
+console.log(today);
