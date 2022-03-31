883964a74ad1136cdfff89354d7ad4f3cf46f72f
DianeBinsztok <diane.binsztok@le-campus-numerique.fr>
2022-03-31 10:53:34 +0200

    exo-4: 0  -  Just playing with string

--- exo-4/script.js
+++ exo-4/script.js
@@ -1,5 +1,7 @@
-// you can write js here
-
-console.log('exo-4');
-
 var secretMessage = ["Learning", "isn't", "about", "what", "you", "get", "easily", "the", "first", "time,", "it's", "about", "what", "you", "can", "figure", "out.", "-2015,", "Chris", "Pine,", "Learn", "JavaScript"];
+
+//Mumuse:
+let display = secretMessage.toString();
+let clean = display.replaceAll(",", " "); 
+let sentence = clean.replace("time ", "time,");
+console.log(sentence);
