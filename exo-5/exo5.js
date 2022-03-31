4f88fac12bbcebb5c52b0eba49b7107f3a6a0560
DianeBinsztok <diane.binsztok@le-campus-numerique.fr>
2022-03-31 14:02:10 +0200

    exo-5: 3-4-5 -  Gathering input's vowels in an array

--- exo-5/exo5.js
+++ exo-5/exo5.js
@@ -3,3 +3,17 @@ const input = prompt("Entrez une phrase");
 
 // 2 - Tableau de référence des voyelles
 const vowels = ["A","Â","À", "E", "É", "È","Ề","Ë", "I","Ï", "O","Ô", "U","Ù", "Y"];
+
+// 3-4-5 - Récupérer les voyelles de l'input dans un tableau:
+const resultArray = [];
+
+function processInput(userInput) {
+        for (i = 0; i < input.length; i++){
+            let letter = input[i].toUpperCase();
+            if (vowels.includes(letter)) {
+            resultArray.push(letter);
+        }
+    }
+}
+processInput(input);
+console.log("Mon tableau de voyelles : ", resultArray);
