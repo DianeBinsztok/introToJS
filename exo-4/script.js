e9715d7fc8f876c5686ca2b3cb66470c13e92943
DianeBinsztok <diane.binsztok@le-campus-numerique.fr>
2022-03-31 11:06:22 +0200

    exo-4: 1  -  Popping array's last element

--- exo-4/script.js
+++ exo-4/script.js
@@ -1,7 +1,17 @@
 var secretMessage = ["Learning", "isn't", "about", "what", "you", "get", "easily", "the", "first", "time,", "it's", "about", "what", "you", "can", "figure", "out.", "-2015,", "Chris", "Pine,", "Learn", "JavaScript"];
 
 //Mumuse:
-let display = secretMessage.toString();
-let clean = display.replaceAll(",", " "); 
-let sentence = clean.replace("time ", "time,");
-console.log(sentence);
+function cleanSentence(array) {
+    let sentence = array.toString().replaceAll(",", " ").replace("time", "time,");
+    return sentence;
+}
+
+
+// Enlever le dernier élément du tableau:
+secretMessage.pop()// renvoie le dernier élément de secretMessage
+console.log(secretMessage);// renvoie le tableau sans son dernier élément
+
+let popped = cleanSentence(secretMessage)
+console.log(popped);
+
+
