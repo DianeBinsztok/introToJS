7ec47a9b9278fe5dc5adde6d432a9d6b64067d71
DianeBinsztok <diane.binsztok@le-campus-numerique.fr>
2022-03-31 11:13:31 +0200

    exo-4: 2 -  Pushing element at array's end

--- exo-4/script.js
+++ exo-4/script.js
@@ -1,17 +1,20 @@
 var secretMessage = ["Learning", "isn't", "about", "what", "you", "get", "easily", "the", "first", "time,", "it's", "about", "what", "you", "can", "figure", "out.", "-2015,", "Chris", "Pine,", "Learn", "JavaScript"];
 
-//Mumuse:
+// 0 - Mumuse:
 function cleanSentence(array) {
     let sentence = array.toString().replaceAll(",", " ").replace("time", "time,");
     return sentence;
 }
 
-
-// Enlever le dernier élément du tableau:
+// 1-  Enlever le dernier élément du tableau:
 secretMessage.pop()// renvoie le dernier élément de secretMessage
 console.log(secretMessage);// renvoie le tableau sans son dernier élément
 
 let popped = cleanSentence(secretMessage)
-console.log(popped);
+console.log("1 - " , popped);
 
-
+// 2 - Ajouter des éléments au tableau:
+secretMessage.push("to", "program");
+console.log(secretMessage);
+let pushed = cleanSentence(secretMessage)
+console.log("2 - ", pushed);
