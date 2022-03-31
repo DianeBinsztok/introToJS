55778a4d273f560b31346d1a67de3af20282d74b
DianeBinsztok <diane.binsztok@le-campus-numerique.fr>
2022-03-31 14:28:23 +0200

    exo-5: 7-8 - Prettify resultArray

--- exo-5/exo5.js
+++ exo-5/exo5.js
@@ -4,7 +4,7 @@ const input = prompt("Entrez une phrase");
 // 2 - Tableau de référence des voyelles
 const vowels = ["A","Â","À", "E", "É", "È","Ề","Ë", "I","Ï", "O","Ô", "U","Ù", "Y"];
 
-// 3-4-5 - Récupérer les voyelles de l'input dans un tableau:
+// 3-4-5-6 - Récupérer les voyelles de l'input dans un tableau:
 const resultArray = [];
 
 function processInput(userInput) {
@@ -20,3 +20,16 @@ function processInput(userInput) {
 }
 processInput(input);
 console.log("Mon tableau de voyelles : ", resultArray);
+
+/* 7 - Vérification de resultArray
+resultArray.forEach(element => {
+    if (vowels.includes(element)) {
+        console.log("resultArray OK");
+    } else {
+        console.log("Somthing wrong in resultArray");
+    }
+});*/
+
+// 8 - Mise en forme du tableau
+prettyArray = resultArray.join().replaceAll(",", " ");
+console.log(prettyArray);
