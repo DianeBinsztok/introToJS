194644f9f94d1311d8522f476f6818e73ced6056
DianeBinsztok <diane.binsztok@le-campus-numerique.fr>
2022-03-31 09:44:38 +0200

    exo-2: correction

--- exo-2/script.js
+++ exo-2/script.js
@@ -3,6 +3,7 @@
 
 // j'instancie la classe Date() qui existe déjà en JS
 const myDate = new Date();
+console.log(myDate);
 
 // Les messages
 const weekMsg = "Nous sommes en semaine!";
@@ -11,19 +12,20 @@ const weekendMsg = "C'est le weekend!";
 // Ce jour:
 let today = myDate.getDay();
 
+
 // Cette heure:
 let now = myDate.getHours();
 
 // Je vérifie si c'est le weekend en fonction du jour et de l'heure
 function checkIfWeekend(day, hour) {
-    if ((day === 7) || (day === 0)) {
+    if ((day === 6) || (day === 0)) {
         console.log(weekendMsg);
     }
-    else if ((day === 6 && (hour >= 17))) {
+    else if ((day === 5 && (hour >= 17))) {
         console.log(weekendMsg);
     } else {
         console.log(weekMsg);
     }
 }
 
-checkIfWeekend(6, 19);
+checkIfWeekend(today, now);
--- exo-3/exo3.js
+++ exo-3/exo3.js
@@ -1,29 +1,66 @@
 // Les choix possibles:
-const choices = ["rock", "paper", "scissors"];
+const choices = [
+    {
+        name: "rock",
+        boss: "paper",
+        biatch: "scissors"
+    
+    },
+    {
+        name: "paper",
+        boss: "scissors",
+        biatch: "rock"  
+    },
+    {
+        name: "scissors",
+        boss: "rock",
+        biatch: "paper"  
+    }
+];
+
 
 // Demander le choix utilisateur:
-var playerInput = prompt("Your choice ( rock, paper, scissors) ?");
+let playerInput = prompt("Your choice ( rock, paper, scissors) ?");
+
 
 // Traiter l'input:
 function getPlayerChoice(input) {
     let lowerCaseInput = input.toLowerCase();
-    if (choices.find(element => element == lowerCaseInput)) {
+    if (choices.find(element => element.name == lowerCaseInput)) {
         playerInput = lowerCaseInput;
         console.log("You chose:", playerInput);
     } else {
         input = prompt("Your choice can only be: rock, paper or scissors.");
         getPlayerChoice(input)
     }
+    return playerInput;
 }
+
 
 // L'ordinateur choisit:
 function getComputerChoice() {
-    let random = Math.round(Math.random(0, 2));
-    let computerChoice = choices[random];
-    console.log("I chose:", computerChoice);
+    let random = Math.floor(Math.random(0, 2)*3);
+    let computerInput = choices[random];
+    console.log("I chose:", computerInput.name);
+    return computerInput;
 }
 
-getPlayerChoice(playerInput);
-getComputerChoice();
+
+// Déterminer le gagnant:
+function findWinner(playerInput, computerInput) {
+    //console.log(playerInput);
+    if (computerInput.name == playerInput) {
+        console.log("It's a draw!");
+    } else if (computerInput.boss == playerInput) {
+        console.log("Congratulations, you win!");
+    } else {
+        console.log("I win!");
+   }
+
+}
+
+const playerChoice = getPlayerChoice(playerInput);
+const computerChoice = getComputerChoice();
+findWinner(playerChoice, computerChoice);
 
 
