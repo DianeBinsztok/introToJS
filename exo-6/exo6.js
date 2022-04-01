2f8b259be9c94a2d6df54598f587ae5f94e26184
DianeBinsztok <diane.binsztok@le-campus-numerique.fr>
2022-04-01 09:30:59 +0200

    exo-6: 1.1 - Display number of cars

--- exo-6/exo6.js
+++ exo-6/exo6.js
@@ -10,8 +10,33 @@ var joeInfo = {
 };
 // ! Ne faites pas de modification au dessus de cette ligne !
 
-// 1. Afficher le nombre de voitures de Joe
+
+//      0 - Encapsuler les infos:
+
+// a - Un titre
+const h2 = document.createElement("h2");
+h2.setAttribute("id", "title");
+h2.innerHTML = "Joe's info :";
+document.getElementById("js_content").appendChild(h2);
+
+// b - Un liste à puces
+const list = document.createElement("ul");
+list.setAttribute("id", "list");
+document.getElementById("js_content").appendChild(list);
+
+
+//      1. Afficher le nombre de voitures de Joe
+const carNumberInfo = joeInfo.cars.length;
+const carNumber = document.createElement("li");
+carNumber.innerHTML = "Number of cars: " + carNumberInfo;
+list.appendChild(carNumber);
+
+
+
 
 // 2. Changer le nombre de salles de bains de Joe : il n'en possède qu'une. Afficher le nouveau nombre de salles de bain.
 
 // 3. Joe vient d'acquérir un garage. Afficher cette nouvelle information. 
+
+
+
