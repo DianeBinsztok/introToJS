00d58bcbf9886ddfcee5e9045630e94c8ac34987
DianeBinsztok <diane.binsztok@le-campus-numerique.fr>
2022-04-01 10:13:34 +0200

    exo-6: 2-3-4-5 - Sport team infos

---
+++ exo-6/exo6-team.js
@@ -0,0 +1,58 @@
+const team = {
+    players: [
+        {
+            name: "John",
+            lastname: "Smith",
+            age: 29
+        },
+        {
+            name: "Paul",
+            lastname: "Trucmuche",
+            age: 24
+        },
+        {
+            name: "Frantz",
+            lastname: "Mann",
+            age: 25
+        },
+        {
+            name: "Mateo",
+            lastname: "Ricci",
+            age: 24
+        },
+        {
+            name: "Said",
+            lastname: "Mabrouk",
+            age: 26
+        }
+
+    ],
+    games: [
+        {
+            opponent: "Broncos",
+            teamPoints: 42,
+            opponentPoints: 27
+        },
+        {
+            opponent: "Stuff",
+            teamPoints: 2000,
+            opponentPoints: 270
+        },
+        {
+            opponent: "Truc",
+            teamPoints: 40,
+            opponentPoints: 35
+        },
+        {
+            opponent: "Cosa",
+            teamPoints: 100,
+            opponentPoints: 200
+        },
+        {
+            opponent: "Dinge",
+            teamPoints: 17,
+            opponentPoints: 35
+        },
+    ]
+};
+
